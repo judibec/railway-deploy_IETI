dc3091215c52cb279f568a902e365b7e685ecb6b
judibec <juanchopolo6954@gmail.com>
2023-02-28 23:03:48 -0500

    :(

---
+++ src/main/java/org/example/ApiController.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String getStatus(){
+        return "Juan Diego Becerra Peña";
+    }
+}
